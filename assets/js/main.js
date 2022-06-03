d266d31a253f1016d80bff113ae44e513022fad9
astorimattia <mattiastori@gmail.com>
2022-06-04 00:06:32 +0700

    chaged back DOMContentLoad to load in main.js; uncommented corra's script in index, and readded the class lazy to the later pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,7 @@
 		});
 
 	// Play initial animations on page load.
-		$window.on('DOMContentLoaded', function() {
+		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
